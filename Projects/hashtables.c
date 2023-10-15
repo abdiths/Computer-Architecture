321c0e3e64f87129c77836c55bd6f7346a337a43
abdi ibrahim <abdiths@gmail.com>
2023-10-15 14:11:22 -0400

    code changes

--- Projects/hashtables.c
+++ Projects/hashtables.c
@@ -1,5 +1,7 @@
 // the hash we are going to use is 64-bit FNV-1a hash (https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function#FNV-1a_hash)
 
+#include "hashtables.h"
+
 #include <assert.h>
 #include <stdint.h>
 #include <stdlib.h>
@@ -48,8 +50,6 @@ void ht_destroy(ht* table) {
 #define FNV_OFFSET 14695981039346656037UL
 #define FNV_PRIME 1099511628211UL
 
-// Return 64-bit FNV-1a hash for key (NUL-terminated). See description:
-// https://en.wikipedia.org/wiki/Fowler–Noll–Vo_hash_function
 static uint64_t hash_key(const char* key) {
     uint64_t hash = FNV_OFFSET;
     for (const char* p = key; *p; p++) {
